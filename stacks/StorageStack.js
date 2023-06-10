5f32dc4ae7b73cc5dc8ece794428f78513064ec4
Austin <atvasquez28@gmail.com>
2023-06-10 17:24:40 -0500

    Adding unit tests

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,4 +1,21 @@
 import { Bucket, Table } from "sst/constructs";
+import { Template } from "aws-cdk-lib/assertions";
+import { initProject } from "sst/project";
+import { App, getStack } from "sst/constructs";
+import { StorageStack } from "../StorageStack";
+import { it } from "vitest";
+
+it("Test StorageStack", async () => {
+  await initProject({});
+  const app = new App({ mode: "deploy" });
+  // WHEN
+  app.stack(StorageStack);
+  // THEN
+  const template = Template.fromStack(getStack(StorageStack));
+  template.hasResourceProperties("AWS::DynamoDB::Table", {
+    BillingMode: "PAY_PER_REQUEST",
+  });
+});
 
 export function StorageStack({ stack, app }) {
   // Create the DynamoDB table
