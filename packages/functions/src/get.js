b12f1253c015cfe2fe6905918e82374c84a2a796
Austin <atvasquez28@gmail.com>
2023-06-11 17:06:48 -0500

    Adding a handler error

--- packages/functions/src/get.js
+++ packages/functions/src/get.js
@@ -10,23 +10,24 @@ function allocMem() {
 // Some faulty code
 dynamoDb.notExist();
 
-export const main = handler(async (event) => {
-  const params = {
-    TableName: Table.Notes.tableName,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
-    Key: {
-      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      noteId: event.pathParameters.id,
-    },
-  };
-
-  const result = await dynamoDb.get(params);
-  if (!result.Item) {
-    throw new Error("Item not found.");
-  }
-
-  // Return the retrieved item
-  return result.Item;
-});
+// Wrong handler function name
+export const main2 = handler(async (event) => {
+    const params = {
+      TableName: Table.Notes.tableName,
+      // 'Key' defines the partition key and sort key of the item to be retrieved
+      // - 'userId': Identity Pool identity id of the authenticated user
+      // - 'noteId': path parameter
+      Key: {
+        userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+        noteId: event.pathParameters.id,
+      },
+    };
+  
+    const result = await dynamoDbLib.call("get", params);
+    if (!result.Item) {
+      throw new Error("Item not found.");
+    }
+  
+    // Return the retrieved item
+    return result.Item;
+  });
