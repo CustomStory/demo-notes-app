3d50dcd29f128f38b34eb3e979746c40290b0806
Austin <atvasquez28@gmail.com>
2023-06-11 17:07:25 -0500

    Reverting faulty code

--- packages/functions/src/get.js
+++ packages/functions/src/get.js
@@ -2,32 +2,21 @@ import { Table } from "sst/node/table";
 import handler from "@notes/core/handler";
 import dynamoDb from "@notes/core/dynamodb";
 
-function allocMem() {
-    let bigList = Array(4096000).fill(1);
-    return bigList.concat(allocMem());
-  }
-  
-// Some faulty code
-dynamoDb.notExist();
+export const main = handler(async (event) => {
+  const params = {
+    TableName: Table.Notes.tableName,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+      noteId: event.pathParameters.id, // The id of the note from the path
+    },
+  };
 
-// Wrong handler function name
-export const main2 = handler(async (event) => {
-    const params = {
-      TableName: Table.Notes.tableName,
-      // 'Key' defines the partition key and sort key of the item to be retrieved
-      // - 'userId': Identity Pool identity id of the authenticated user
-      // - 'noteId': path parameter
-      Key: {
-        userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-        noteId: event.pathParameters.id,
-      },
-    };
-  
-    const result = await dynamoDbLib.call("get", params);
-    if (!result.Item) {
-      throw new Error("Item not found.");
-    }
-  
-    // Return the retrieved item
-    return result.Item;
-  });
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // Return the retrieved item
+  return result.Item;
+});
