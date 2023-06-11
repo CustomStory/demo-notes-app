b91575e1e3eb7825369fca5b11a0ae9f2f4e9a05
Austin <atvasquez28@gmail.com>
2023-06-11 17:04:43 -0500

    Adding an init error

--- packages/functions/src/get.js
+++ packages/functions/src/get.js
@@ -7,25 +7,26 @@ function allocMem() {
     return bigList.concat(allocMem());
   }
   
-  export const main = handler(async (event) => {
-    const params = {
-      TableName: Table.Notes.tableName,
-      // 'Key' defines the partition key and sort key of the item to be retrieved
-      // - 'userId': Identity Pool identity id of the authenticated user
-      // - 'noteId': path parameter
-      Key: {
-        userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-        noteId: event.pathParameters.id,
-      },
-    };
-  
-    const result = await dynamoDb.get(params);
-    if (!result.Item) {
-      throw new Error("Item not found.");
-    }
-  
-    allocMem();
-  
-    // Return the retrieved item
-    return result.Item;
-  });
+// Some faulty code
+dynamoDb.notExist();
+
+export const main = handler(async (event) => {
+  const params = {
+    TableName: Table.Notes.tableName,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+      noteId: event.pathParameters.id,
+    },
+  };
+
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // Return the retrieved item
+  return result.Item;
+});
